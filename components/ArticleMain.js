b8aefdf4f3c5bda454d30c85c8dddce66e71f245
asimrauf-sybrid <102941062+asimrauf-sybrid@users.noreply.github.com>
2022-08-14 19:49:21 +0500

    refactored

--- components/ArticleMain.js
+++ components/ArticleMain.js
@@ -29,71 +29,66 @@ const styles = {
   subtitle: `font-mediumSerifItalic text-[1.4rem] text-[#292929]`,
   articleText: `font-medimuSerif text-[1.4rem] text-[#292929]`,
 };
-const ArticleMain = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.content}>
-        <div className={styles.postHeaderContainer}>
-          <div className={styles.authorContainer}>
-            <div className={styles.authorProfileImageContainer}>
-              <Image className={styles.image} src={Profile} alt='Profile' />
-            </div>
-            <div className={styles.column}>
-              <div>Asim Rauf</div>
-              <div className={styles.postDetails}>
-                <span>June 15 ~ 7 min read ~ </span>
-                <span className={styles.listenButton}>
-                  <AiFillPlayCircle />
-                  Listen
-                </span>
-              </div>
-            </div>
+const ArticleMain = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.content}>
+      <div className={styles.postHeaderContainer}>
+        <div className={styles.authorContainer}>
+          <div className={styles.authorProfileImageContainer}>
+            <Image className={styles.image} src={Profile} alt='Profile' />
           </div>
-          <div className={styles.socials}>
-            <IoLogoTwitter />
-            <FaFacebook />
-            <GrLinkedin />
-            <HiOutlineLink />
-            <div className={styles.space} />
-            <BiBookmarks />
-            <FiMoreHorizontal />
+          <div className={styles.column}>
+            <div>Asim Rauf</div>
+            <div className={styles.postDetails}>
+              <span>June 15 ~7 min read ~ </span>
+              <span className={styles.listenButton}>
+                <AiFillPlayCircle />
+                Listen
+              </span>
+            </div>
           </div>
         </div>
-        <div className={styles.articleMainContainer}>
-          <div className={styles.bannerContiner}>
-            <Image
-              src={Banner}
-              alt='Banner'
-              height={100}
-              width={100}
-              className={styles.image}
-            />
-          </div>
-          <h1 className={styles.title}>
-            7 Free Tools That Will Make You More Productive In 2022
-          </h1>
-          <h4 className={styles.subtitle}>
-            <div>Asim Rauf, June 15, 2022</div>
-            <div>Brief: Productivity is a skill that can be learned.</div>
-          </h4>
-          <div className={styles.articleText}>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32.
-          </div>
+        <div className={styles.socials}>
+          <IoLogoTwitter />
+          <FaFacebook />
+          <GrLinkedin />
+          <HiOutlineLink />
+          <div className={styles.space} />
+          <BiBookmarks />
+          <FiMoreHorizontal />
+        </div>
+      </div>
+      <div className={styles.articleMainContainer}>
+        <div className={styles.bannerContiner}>
+          <Image
+            src={Banner}
+            alt='Banner'
+            height={100}
+            width={100}
+            className={styles.image}
+          />
+        </div>
+        <h1 className={styles.title}>
+          7 Free Tools That Will Make You More Productive In 2022
+        </h1>
+        <h4 className={styles.subtitle}>
+          <div>Asim Rauf, June 15, 2022</div>
+          <div>Brief: Productivity is a skill that can be learned.</div>
+        </h4>
+        <div className={styles.articleText}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ArticleMain;
